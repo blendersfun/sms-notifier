94cd56af61fb4f3816d0a159c227f1f063fe0400
Aaron Moore <two.a.ron@gmail.com>
2016-11-18 10:19:31 -0800

    dockerize fixes

--- rebuild-and-run.sh
+++ rebuild-and-run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker rm -f sms-notifier && \
-    docker build -t sms-notifier . && \
-    docker run -d --name sms-notifier -v /Users/mooreaa/repos/sms-notifier/secrets:/usr/src/app/secrets sms-notifier && \
-    docker exec -it sms-notifier /bin/bash
+sudo docker rm -f sms-notifier && \
+	sudo docker build -t sms-notifier . && \
+	sudo docker run -d --name sms-notifier -v $PWD/secrets:/usr/src/app/secrets sms-notifier && \
+	sudo docker exec -it sms-notifier /bin/bash
